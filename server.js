5066f43509f1fc4c3dd6ee0ddaef7cbc4747e78f
Will Darkins <willdarkins@gmail.com>
2022-02-03 21:15:22 -0800

    connected server to mongoose database

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+// importing mongoose to connect with server on start up
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,5 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
